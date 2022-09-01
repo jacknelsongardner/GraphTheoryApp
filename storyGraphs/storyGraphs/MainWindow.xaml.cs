c7c4c5ca56b00e87e4970d3d4fc77e2bc40a2f3e
jgardner <jack.gardner@wsu.edu>
2022-09-01 15:29:04 -0700

    added functionality to drag and drop rectangles with a left click

--- storyGraphs/storyGraphs/MainWindow.xaml.cs
+++ storyGraphs/storyGraphs/MainWindow.xaml.cs
@@ -25,21 +25,39 @@ namespace storyGraphs
             InitializeComponent();
         }
 
+        //whether the mouse is being dragged
+        bool mouseDragging;
+
         Brush StandardBrush;
         Random r = new Random();
 
         //for incrementing the ID we give in the dictionary
-        int nextRectangleId = 0;
+        //int nextRectangleId = 0;
+
+
+
+
         //dictionary for storing all the rectangles, so they can be called and deleted later on
         Dictionary<Rectangle,Node> rectanglesOnCanvas = new Dictionary<Rectangle,Node>();
         Dictionary<Line, Edge> linesOnCanvas = new Dictionary<Line, Edge>();
-        
-        LinkedList<Node> nodesOnCanvas = new LinkedList<Node>();
-        LinkedList<Edge> edgesOnCanvas = new LinkedList<Edge>();
+
         
 
-        //when the user clicks with the left mousebutton on the Graph/Node canvas
-        private void MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+
+        /*
+        LinkedList<Node> nodesOnCanvas = new LinkedList<Node>();
+        LinkedList<Edge> edgesOnCanvas = new LinkedList<Edge>();
+        */
+
+
+
+
+
+
+        //WPF EVENTS
+
+        //when the user clicks with the RIGHT mousebutton on the Graph/Node canvas
+        private void MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
             
                 // If we're clicking on a rectangle
@@ -49,9 +67,6 @@ namespace storyGraphs
                     Rectangle activeRec = (Rectangle)e.OriginalSource;
                     NodesEdgesCanvas.Children.Remove(activeRec); // find the rectangle and remove it from the canvas
                     rectanglesOnCanvas.Remove(activeRec);
-                    
-                
-                    Console.WriteLine();
                     
 
 
@@ -75,10 +90,7 @@ namespace storyGraphs
                     };
 
                     rectanglesOnCanvas.Add(newRec,new Node());
-                    nextRectangleId++;
                     
-
-
 
                     //getting mouse x and y for rectangle placement
                     Canvas.SetLeft(newRec, Mouse.GetPosition(NodesEdgesCanvas).X); // set the left position of rectangle to mouse X
@@ -86,15 +98,69 @@ namespace storyGraphs
 
                     NodesEdgesCanvas.Children.Add(newRec); // add the new rectangle to the canvas
 
-                    Console.WriteLine();
 
             }
 
         }
 
+        //when the user clicks with the LEFT mousebutton on the GRAPHNode canvas
+        private void MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            // If we're clicking on a rectangle
+            if (e.OriginalSource is Rectangle)
+            {
+                //deleting the rectangle
+                Rectangle activeRec = (Rectangle)e.OriginalSource;
+                mouseDragging = true;
+                activeRec.CaptureMouse();
 
 
+            }
 
+
+        }
+
+        //when the user clicks with the LEFT mousebutton on the GRAPHNode canvas
+        private void MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            // If we're clicking on a rectangle
+            if (e.OriginalSource is Rectangle)
+            {
+                //deleting the rectangle
+                Rectangle activeRec = (Rectangle)e.OriginalSource;
+                mouseDragging = false;
+                activeRec.ReleaseMouseCapture();
+
+
+            }
+
+        }
+
+        //when the users mouse moves
+        private void MouseMove(object sender, MouseEventArgs e)
+        {
+            //if we're moving a rectangle
+            if (e.OriginalSource is Rectangle)
+            {
+               
+                //if the mouse has a rectangle in its grasp
+                if (!mouseDragging) return;
+                {
+                    Rectangle activeRec = (Rectangle)e.OriginalSource;
+
+                    // get the position of the mouse relative to the Canvas
+                    var mousePos = e.GetPosition(NodesEdgesCanvas);
+
+                    // put the rectangle on the mouse
+                    double left = mousePos.X - (activeRec.ActualWidth / 2);
+                    double top = mousePos.Y - (activeRec.ActualHeight / 2);
+                    Canvas.SetLeft(activeRec, left);
+                    Canvas.SetTop(activeRec, top);
+
+                }
+            }
+        }
 
 
     }
